60be83e251286e612488f1ab7bea514111ac0601
danielbesen <42474185+danielbesen@users.noreply.github.com>
2022-02-20 19:21:13 -0300

    Added category entity and DAO

---
+++ FidelityLibrary/Entity/Categories/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FidelityLibrary.Entity.Categories
+{
+    [Table("categoria", Schema = "public")]
+    public class Category
+    {
+        [Key, Column("id_categoria")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("ds_categoria")]
+        public string Name { get; set; }
+
+        [Column("dt_inclusao")]
+        public DateTime InsertDate { get; set; } = DateTime.UtcNow;
+
+        [Column("dt_alteracao")]
+        public DateTime? AlterDate { get; set; }
+    }
+}
---
+++ FidelityLibrary/Persistance/CategoryDAO/CategoryDAO.cs
@@ -0,0 +1,15 @@
+﻿using FidelityLibrary.Entity.Categories;
+using FidelityLibrary.Persistance.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FidelityLibrary.Persistance.CategoryDAO
+{
+    public class CategoryDAO : GenericDAO<Category, int>
+    {
+
+    }
+}

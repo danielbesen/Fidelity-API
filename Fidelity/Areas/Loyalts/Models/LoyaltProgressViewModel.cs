e70d08a3d109f59e76302d9ffdeab6b75ddbb282
danielbesen <42474185+danielbesen@users.noreply.github.com>
2022-04-05 21:11:09 -0300

    LoyaltProgress

---
+++ Fidelity/Areas/Loyalts/Models/LoyaltProgressViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fidelity.Areas.Loyalts.Models
+{
+    public class LoyaltProgressViewModel
+    {
+        public int? Id { get; set; }
+        public int ClientId { get; set; }
+        public int id_checkpoint { get; set; }
+        public int Points { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ FidelityLibrary/Persistance/LoyaltProgressDAO/LoyaltProgressDAO.cs
@@ -0,0 +1,16 @@
+﻿using FidelityLibrary.Entity.Checkpoints;
+using FidelityLibrary.Entity.Loyalts;
+using FidelityLibrary.Persistance.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FidelityLibrary.Persistance.LoyaltProgressDAO
+{
+    public class LoyaltProgressDAO : GenericDAO<LoyaltProgress, int?>
+    {
+
+    }
+}
